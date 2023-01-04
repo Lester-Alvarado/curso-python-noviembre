3f04bbca1bdb4cf57163a0addc377e651c760c39
Lester Alvarado <lesteraam@gmail.com>
2023-01-04 01:13:45 -0600

    Agregar estructura de API

---
+++ api.py
@@ -0,0 +1,15 @@
+# API - stateless
+# request
+# URL - dominio - https://google.com - https://189.205.65.69
+# URL - path - https://google.com/photos
+# metodo - GET - POST - PUT - DELETE
+# body - {
+#   "name": "Lester",
+#   "email": "email@ejemplo.com"
+# }
+# headers - Authorization: abc-123, Content-Type: application/jpg. Accept: application/excel
+# Path parameters /photos/1
+# Query params /friends?gender=male&adults=yes
+
+# enpoint
+# response
