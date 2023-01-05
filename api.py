6f352c1641cfa35ad9577d9c6ab4bdb7a1283d20
Lester Alvarado <lesteraam@gmail.com>
2023-01-05 10:17:28 -0600

    Agregar ejemplos de solicitudes de API

--- api.py
+++ api.py
@@ -13,3 +13,16 @@
 
 # enpoint
 # response
+# Body
+# Headers
+# Codigos HTTP - 100 200 300 400
+
+import requests 
+
+response = requests.get("https://swapi.dev/api/people", data={"page": 2})
+print(response)
+print(response.status_code)
+# print(response.content)
+print(response.headers)
+data = response.json()
+print(data ["results"][0]["name"])
